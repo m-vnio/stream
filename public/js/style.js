41135bee5482ead9c9bd54a34e0df62ab92eac5f
victor salas <victor001.sp@gmail.com>
2023-10-12 18:38:22 -0500

    theme dark fix

--- public/js/style.js
+++ public/js/style.js
@@ -34,14 +34,14 @@ export default ()=>{
     const theme = localStorage.getItem('theme')
 
     const themeLight = {
-        'color-background' : '#EDF3FB',   
+        'color-background' : '#fffff',   
         'color-item' : '#ffffff',   
         'color-letter' : '#1c1c1e',
         'filter-img'   : 'initial'
     }
 
     const themeDark  = {
-        'color-background' : '#1c1c1e',   
+        'color-background' : '#000000',   
         'color-item' : '#2C2C2E',   
         'color-letter' : '#EDF3FB',
         'filter-img'   :'invert(82%) sepia(99%) saturate(0%) hue-rotate(102deg) brightness(111%) contrast(100%)'
